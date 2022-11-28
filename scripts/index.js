b8e98b043cae4bf5659a0825a6e84646584e33cd
Student <student@StudentiMac-33.local>
2022-11-28 15:40:36 -0500

    added functionality for adding and removing number of people to split

--- scripts/index.js
+++ scripts/index.js
@@ -17,5 +17,26 @@ const ADD_TIP = () => {
     })
 }
 
+const MINUS_SPLIT = () => {
+    let minus = document.getElementById("subtract-split")
+    let tip = document.getElementById("split")
+    minus.addEventListener("click", (e)=> {
+        e.preventDefault();
+        tip.value - 1 == -1 ? 0 : tip.value -= 1
+    })
+}
+
+const ADD_SPLIT = () => {
+    let add = document.getElementById("add-split")
+    let tip = document.getElementById("split")
+    add.addEventListener("click", (e)=> {
+        e.preventDefault();
+        tipValue = parseInt(tip.value) 
+        tip.value = tipValue + 1
+    })
+}
+
+MINUS_SPLIT();
+ADD_SPLIT();
 ADD_TIP();
 MINUS_TIP();

ac4ef901a76be2d61f90f2b20e3a20bea649c921
Shehu-Muhammad <shehumuhammad@my.ccsu.edu>
2022-11-29 23:43:32 -0500

    [UPDATE] added functionality to make calculate button show tip calculations

--- scripts/index.js
+++ scripts/index.js
@@ -19,20 +19,20 @@ const ADD_TIP = () => {
 
 const MINUS_SPLIT = () => {
     let minus = document.getElementById("subtract-split")
-    let tip = document.getElementById("split")
+    let split = document.getElementById("split")
     minus.addEventListener("click", (e)=> {
         e.preventDefault();
-        tip.value - 1 == -1 ? 0 : tip.value -= 1
+        split.value - 1 == -1 ? 0 : split.value -= 1
     })
 }
 
 const ADD_SPLIT = () => {
     let add = document.getElementById("add-split")
-    let tip = document.getElementById("split")
+    let split = document.getElementById("split")
     add.addEventListener("click", (e)=> {
         e.preventDefault();
-        tipValue = parseInt(tip.value) 
-        tip.value = tipValue + 1
+        splitValue = parseInt(split.value) 
+        split.value = splitValue + 1
     })
 }
 
@@ -40,3 +40,45 @@ MINUS_SPLIT();
 ADD_SPLIT();
 ADD_TIP();
 MINUS_TIP();
+
+const CALCULATE_TIP = (tip) => {
+    let amount = document.getElementById("amount").value
+    let split = document.getElementById("split").value
+    
+    let total = Math.ceil((parseInt(amount) * (parseInt(tip)/100 + 1)) / parseInt(split))
+    return total;
+}
+
+const CALCULATE_TOTAL = () => {
+    let fifteenPercent = CALCULATE_TIP(15)
+    let twentyPercent = CALCULATE_TIP(20)
+    let twentyFivePercent = CALCULATE_TIP(25)
+    let tip = document.getElementById("tip").value
+    let split = document.getElementById("split").value
+    
+    let fifteen = document.getElementById("fifteen-percent-tip")
+    let twenty = document.getElementById("twenty-percent-tip")
+    let twentyFive = document.getElementById("twenty-five-percent-tip")
+    let custom = document.getElementById("custom-tip")
+    
+    fifteen.innerHTML = fifteenPercent
+    twenty.innerHTML = twentyPercent
+    twentyFive.innerHTML = twentyFivePercent
+    custom.innerHTML = CALCULATE_TIP(parseInt(tip))
+    
+    
+    document.getElementById("tip-split").innerHTML = split
+    document.getElementById("result").style.display = "block";
+    
+}
+
+const CALCULATE_BUTTON = () => {
+    let calculateButton = document.getElementById("calculate")
+    
+    calculateButton.addEventListener("click", (e)=>{
+        e.preventDefault()
+        CALCULATE_TOTAL();
+    })
+}
+
+CALCULATE_BUTTON();
